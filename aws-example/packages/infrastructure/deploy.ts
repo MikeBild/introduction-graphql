87e53dc4ea52bf5aeeccda9727db6695283d4cd1
Mike Bild <mike@mikebild.com>
2022-01-13 16:25:36 +0100

    more

--- aws-example/packages/infrastructure/deploy.ts
+++ aws-example/packages/infrastructure/deploy.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-import { App } from 'aws-cdk-lib';
+import { App, Tags } from 'aws-cdk-lib';
 import { AppSyncStack } from './stacks/appsync';
 import { LambdaStack } from './stacks/lambdas';
 
 const envName = 'Prod';
 
 const app = new App();
+Tags.of(app).add('Stage', envName);
+
 const { articlesLambda } = new LambdaStack(app, 'LambdaStack', { envName });
 new AppSyncStack(app, 'AppSyncStack', { articlesLambda });
